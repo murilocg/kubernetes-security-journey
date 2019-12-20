eae8e33bf32cfa83db6fa0cd8949cbcc77e0309c
Murilo Costa <murilo.costa@dtidigital.com.br>
2019-12-20 16:12:26 -0300

    default parameters to initialize

--- us-east-1/prod/kubernetes-cluster/charts/kong/kubernetes-kong.tf
+++ us-east-1/prod/kubernetes-cluster/charts/kong/kubernetes-kong.tf
@@ -18,9 +18,10 @@ provider "kubernetes" {
 provider "helm" {
   version        = "~> 0.9"
   install_tiller = false
-  client_certificate     = file("../../kubernetes-tiller/.secret/helm_client_tls_public_cert_pem.pem")
-  client_key             = file("../../kubernetes-tiller/.secret/helm_client_tls_private_key_pem.pem")
-  ca_certificate = file("../../kubernetes-tiller/.secret/helm_client_tls_ca_cert_pem.pem")
+  helm_home = "${var.helm_home}"
+  # client_certificate     = file("../../kubernetes-tiller/.secret/helm_client_tls_public_cert_pem.pem")
+  # client_key             = file("../../kubernetes-tiller/.secret/helm_client_tls_private_key_pem.pem")
+  # ca_certificate = file("../../kubernetes-tiller/.secret/helm_client_tls_ca_cert_pem.pem")
   namespace = "tiller"
 
   kubernetes {
